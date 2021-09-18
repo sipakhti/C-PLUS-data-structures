f42531c40bffca11e3babc7f8747db949d83fb38
Umer Mehmood <50553904+sipakhti@users.noreply.github.com>
2021-09-19 02:38:48 +0500

    added print_half_pyramid() and refactored print_inverted_half_pyramid() to look cooler?

--- awesome patterns.cpp
+++ awesome patterns.cpp
@@ -6,7 +6,8 @@ namespace colors {
     std::string OKBLUE = "\033[94m";
     std::string OKGREEN = "\033[92m";
     std::string WARNING = "\033[93m";
-    std::string FAIL = "\033[91m";
+    std::string RED_FG = "\033[91m";
+    std::string RED_BG = "\033[101m";
     std::string ENDC = "\033[0m";
     std::string BOLD = "\033[1m";
     std::string UNDERLINE = "\033[4m";
@@ -22,21 +23,24 @@ void print_rectangle(int, int);
 bool is_boundry(int,int,int);
 void print_hollow_rectangle(int,int);
 void print_inverted_half_pyramid(int);
+void print_half_pyramid(int);
 
 int main() {
-    cout << FAIL;
+    // cout << FAIL;
     print_rectangle(5,5);
-    cout << OKBLUE;
+    // cout << OKBLUE;
     print_hollow_rectangle(5,5);
-    cout << OKGREEN;
+    // cout << OKGREEN;
     print_inverted_half_pyramid(5);
-    cout << ENDC;
+    // cout << WARNING;
+    print_half_pyramid(5);
+    // cout << ENDC;
     return 0;
 }
 
 void add_horizontal_rule(){
     cout << endl;
-    cout << colors::FAIL << "< ---------------------------------------- >" << colors::ENDC << endl;
+    cout << colors::RED_FG << "< ---------------------------------------- >" << colors::ENDC << endl;
     cout << endl;
 }
 
@@ -70,10 +74,26 @@ void print_hollow_rectangle(int rows, int columns){
 }
 
 void print_inverted_half_pyramid(int height) {
-    for (int i = height; i >= 0; i--){
+    // outer loop iterates over the rows
+    for (int i = height; i > 0; i--){
         for (size_t j = 0; j < i; j++)
-            cout << " * ";
+            cout << RED_FG << " * " << ENDC;
+        for (size_t k = 0; k < (height - i); k++)
+            cout << RED_BG << "   " << ENDC;
         cout << endl;
     }
 }
 
+void print_half_pyramid(int height){
+    for (int i = height; i > 0; i--)
+    {
+        int spaces = i - 1;
+        int stars = height - spaces;
+        for (size_t j = 0; j < spaces; j++)
+            cout << RED_BG << "   " << ENDC;
+        for (size_t k = 0; k < stars; k++)
+            cout << RED_FG << " * " << ENDC;
+        cout << endl;
+    }
+    
+}
