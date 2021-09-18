6e6c4c8c6d031fe7c0991015f48857cf9628d781
Umer Mehmood <50553904+sipakhti@users.noreply.github.com>
2021-09-19 01:59:09 +0500

    added a new color namespace for console formatting, refactored some code, added function to print a horizontal divider

--- awesome patterns.cpp
+++ awesome patterns.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+namespace colors {
+    string HEADER = "\033[95m";
+    string OKBLUE = "\033[94m";
+    string OKGREEN = "\033[92m";
+    string WARNING = "\033[93m";
+    string FAIL = "\033[91m";
+    string ENDC = "\033[0m";
+    string BOLD = "\033[1m";
+    string UNDERLINE = "\033[4m";
+}
+
 void print_rectangle(int, int);
+bool is_boundry(int,int,int);
+void print_hollow_rectangle(int,int);
+void add_horizontal_rule();
 
 int main() {
-  print_rectangle(5,5);
-  
-  return 0;
+    cout << colors::WARNING;
+    print_rectangle(5,5);
+    add_horizontal_rule();
+    cout << colors::OKBLUE;
+    print_hollow_rectangle(5,5);
+    
+    cout << colors::ENDC;
+    return 0;
+}
+
+void add_horizontal_rule(){
+    cout << endl;
+    cout << colors::FAIL << "< ---------------------------------------- >" << colors::ENDC << endl;
+    cout << endl;
 }
 
 void print_rectangle(int rows, int columns){
@@ -18,14 +44,17 @@ void print_rectangle(int rows, int columns){
     }
 }
 
+bool is_boundry(int val, int upper_bound, int lower_bound) {
+    return val == lower_bound || val == --upper_bound;
+}
 
 void print_hollow_rectangle(int rows, int columns){
     for (size_t i = 0; i < rows; i++){
         for (size_t j = 0; j < columns; j++){
-            if (i == 0 || i == rows-1)
+            if (is_boundry(i,rows, 0))
                 cout << " * ";
             else {
-                if (j == 0 || j == columns-1)
+                if (is_boundry(j, columns, 0))
                     cout << " * ";
                 else 
                     cout << "   ";
