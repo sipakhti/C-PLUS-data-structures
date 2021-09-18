55fa8fd73082a1027d8e72a584e5aef12e0d49ce
Umer Mehmood <50553904+sipakhti@users.noreply.github.com>
2021-09-19 02:07:45 +0500

    added print_inverted_half_pyramid()

--- awesome patterns.cpp
+++ awesome patterns.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 namespace colors {
-    string HEADER = "\033[95m";
-    string OKBLUE = "\033[94m";
-    string OKGREEN = "\033[92m";
-    string WARNING = "\033[93m";
-    string FAIL = "\033[91m";
-    string ENDC = "\033[0m";
-    string BOLD = "\033[1m";
-    string UNDERLINE = "\033[4m";
+    std::string HEADER = "\033[95m";
+    std::string OKBLUE = "\033[94m";
+    std::string OKGREEN = "\033[92m";
+    std::string WARNING = "\033[93m";
+    std::string FAIL = "\033[91m";
+    std::string ENDC = "\033[0m";
+    std::string BOLD = "\033[1m";
+    std::string UNDERLINE = "\033[4m";
 }
 
+
+using namespace std;
+using namespace colors;
+
+
+void add_horizontal_rule();
 void print_rectangle(int, int);
 bool is_boundry(int,int,int);
 void print_hollow_rectangle(int,int);
-void add_horizontal_rule();
+void print_inverted_half_pyramid(int);
 
 int main() {
-    cout << colors::WARNING;
+    cout << FAIL;
     print_rectangle(5,5);
-    add_horizontal_rule();
-    cout << colors::OKBLUE;
+    cout << OKBLUE;
     print_hollow_rectangle(5,5);
-    
-    cout << colors::ENDC;
+    cout << OKGREEN;
+    print_inverted_half_pyramid(5);
+    cout << ENDC;
     return 0;
 }
 
@@ -64,3 +68,12 @@ void print_hollow_rectangle(int rows, int columns){
         cout << endl;
     }
 }
+
+void print_inverted_half_pyramid(int height) {
+    for (int i = height; i >= 0; i--){
+        for (size_t j = 0; j < i; j++)
+            cout << " * ";
+        cout << endl;
+    }
+}
+
